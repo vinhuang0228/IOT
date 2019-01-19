40aecb6285a0d93665d657c9ba791e608cdcd45d
Tina Huang <vinhuang0228@gmail.com>
2019-01-19 18:13:12 +0800

    Updates

---
+++ precache-manifest.7abe7749b3c6439efd505f74cc339992.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "72d49279529eb60ec652",
+    "url": "/IOT/static/js/runtime~main.72d49279.js"
+  },
+  {
+    "revision": "513b07da8670ac08fcb0",
+    "url": "/IOT/static/js/main.513b07da.chunk.js"
+  },
+  {
+    "revision": "e7f085a106dd0e8c6295",
+    "url": "/IOT/static/js/1.e7f085a1.chunk.js"
+  },
+  {
+    "revision": "e7f085a106dd0e8c6295",
+    "url": "/IOT/static/css/1.cf9fe760.chunk.css"
+  },
+  {
+    "revision": "9df938ab55f548ebcffaa9bcc30c8442",
+    "url": "/IOT/index.html"
+  }
+];
